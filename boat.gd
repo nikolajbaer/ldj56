a6b277b18d6a956d559aa611561bdbf1cc4eab93
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-10-05 15:41:52 -0600

    stow eggs better

--- boat.gd
+++ boat.gd
@@ -2,14 +2,14 @@ extends AnimatableBody3D
 
 signal full
 
+const EGG_WIDTH=0.6
 var egg_count = 0
 
 func _on_catch_area_body_entered(body: Node3D) -> void:
 	if body is Egg:
 		body.stow()
-		body.get_parent().remove_child(body)
-		body.position = $EggStart.position + Vector3(egg_count%2 * 3,0,floor(egg_count/6)*3)
-		add_child(body)
+		body.reparent(self)
+		body.position = $EggStart.position + Vector3(egg_count%2 * EGG_WIDTH,0,floor(egg_count/6)*EGG_WIDTH)
 		egg_count += 1
 		if egg_count == 12:
 			full.emit()
