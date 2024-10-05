e5be2c2398a6e2acc0ffb83e9c129b97fa80ea5d
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-10-05 09:22:13 -0600

    full commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Ludum Dare 56 "Tiny Creatures"
+
+TODO Gameplay:
+* Make creatures smartly follow trampoline
+* when get to trampoline, chomp on it
+* creatures chomped on slow down trampoline
+* drop powerup to release creatures?
+
+
+
+
+
--- creature.gd
+++ creature.gd
@@ -19,26 +19,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
+		# random cute little hop
 		if randf() < 0.01:
 			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var dir = Vector3(0,0,0)
+	# look at our target at the same Y-plane
 	var target_same_plane = Vector3(target.position.x,self.position.y,target.position.z)
-	if target:
-		var dist_to = target_same_plane - self.position
-		if dist_to.length() > 5:
-			dir = dist_to
-			self.look_at(target_same_plane)
-	if dir.length():
-		var direction := (transform.basis * Vector3(dir.x, 0, dir.z)).normalized()
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
+	self.look_at(target_same_plane)
+	#var dist_to = target_same_plane - self.position
+		
+	var direction := -transform.basis.z.normalized()
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
 
 	move_and_slide()
