1af5972029cb1c8c3fbdcefe84301e5dda3e41f5
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-10-05 16:22:22 -0600

    fix boat egg layout, reduce crack timer

--- boat.gd
+++ boat.gd
@@ -9,7 +9,7 @@ func _on_catch_area_body_entered(body: Node3D) -> void:
 	if body is Egg and not body.freeze:
 		body.stow()
 		body.reparent(self)
-		body.position = $EggStart.position + Vector3(egg_count%2 * EGG_WIDTH,0,floor(egg_count/6)*EGG_WIDTH)
+		body.position = $EggStart.position + Vector3(egg_count%2 * EGG_WIDTH,0,floor(egg_count/2)*EGG_WIDTH)
 		egg_count += 1
 		if egg_count == 12:
 			full.emit()
