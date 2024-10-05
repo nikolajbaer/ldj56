c614ff865e954cc8d6bdf0582ad57ceaddecfdda
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-10-05 15:51:56 -0600

    add egg timer

--- boat.gd
+++ boat.gd
@@ -6,7 +6,7 @@ const EGG_WIDTH=0.6
 var egg_count = 0
 
 func _on_catch_area_body_entered(body: Node3D) -> void:
-	if body is Egg:
+	if body is Egg and not body.freeze:
 		body.stow()
 		body.reparent(self)
 		body.position = $EggStart.position + Vector3(egg_count%2 * EGG_WIDTH,0,floor(egg_count/6)*EGG_WIDTH)
--- egg.gd
+++ egg.gd
@@ -2,9 +2,10 @@ extends RigidBody3D
 class_name Egg
 
 signal hit_ground
-
+@onready var timer = $Timer
 const CRACK_SPEED = 6
 var last_vel
+var crack_triggered = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,11 +18,15 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node) -> void:
 	print("Egg hit ",body.name," at ",last_vel)
 
-	if body.name == "ground" and last_vel > CRACK_SPEED:
-		var ground_pos = Vector3(position.x,position.y,position.z)
-		hit_ground.emit(ground_pos)
-		self.queue_free()
+	if body.name == "ground" and last_vel > CRACK_SPEED and not crack_triggered:
+		timer.start()
+		crack_triggered = true
 
 func stow():
 	self.freeze = true
 	rotation = Vector3(0,0,0)
+
+func _on_timer_timeout() -> void:
+	var ground_pos = Vector3(position.x,position.y,position.z)
+	hit_ground.emit(ground_pos)
+	self.queue_free()
